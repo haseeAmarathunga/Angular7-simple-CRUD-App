2b526bdee22b035779f55a741713baf1e1c730bc
haseeAmarathunga <haseeamarathunga@gmail.com>
2019-02-10 17:02:41 +0530

    Update UI with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CustomerComponent } from './customer/customer.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'customer' , component: CustomerComponent},
+  {path: 'customerlist' , component: CustomerListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [CustomerComponent , CustomerListComponent ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatButtonModule , MatCheckboxModule} from '@angular/material';
-
 import { AngularFireModule} from 'angularfire2';
 import { AngularFireDatabaseModule} from 'angularfire2/database';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule , routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CustomerComponent } from './customer/customer.component';
-import { CustomerListComponent } from './customer-list/customer-list.component';
 import { ReactiveFormsModule , FormsModule } from '@angular/forms';
 import { CustomerService } from './shared/customer.service';
 import { environment } from '../environments/environment';
 import { HeaderComponent } from './inc/header/header.component';
-
+import { FooterComponent } from './inc/footer/footer.component';
+import { MatDatepickerModule } from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent,
-    CustomerComponent,
-    CustomerListComponent,
+    routingComponents,
     HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
@@ -29,6 +27,7 @@ import { HeaderComponent } from './inc/header/header.component';
     FormsModule,
     MatButtonModule,
     MatCheckboxModule,
+    MatDatepickerModule
 
   ],
   providers: [CustomerService],
